ffe1013336b860b97445cc96a2320ae92f2c562f
Shubham Shrivastava <128039947+Shubhhammmm@users.noreply.github.com>
2023-06-11 22:18:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,36 @@
-import React from "react";
-import "../styles/App.css";
-import Loader from "./Loader";
+import React from 'react';
+import '../styles/App.css';
+import Loader from './Loader';
 
 const LoadingStatus = {
-  NOT_STARTED: "NOT_STARTED",
-  IN_PROGRESS: "IN_PROGRESS",
-  SUCCESS: "SUCCESS",
+  NOT_STARTED: 'NOT_STARTED',
+  IN_PROGRESS: 'IN_PROGRESS',
+  SUCCESS: 'SUCCESS',
 };
 
 const App = () => {
-  const BASE_URL = "https://content.newtonschool.co/v1/pr/main/users";
+  const BASE_URL = 'https://content.newtonschool.co/v1/pr/main/users';
   const [userId, setUserId] = React.useState(1);
   const [isLoading, setIsLoading] = React.useState(LoadingStatus.NOT_STARTED);
   const [userData, setUserData] = React.useState({
-    id: "",
-    email: "",
-    name: "",
-    phone: "",
-    webiste: "",
+    id: '',
+    email: '',
+    name: '',
+    phone: '',
+    webiste: '',
   });
 
-  const handleOnClick = () => {};
+  const handleOnClick = () => {
+    setIsLoading(LoadingStatus.IN_PROGRESS);
+    setTimeout(() => {
+      fetch(`${BASE_URL}/${userId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setUserData(data);
+          setIsLoading(LoadingStatus.SUCCESS);
+        });
+    }, 2000);
+  };
 
   const onChangeHandler = (event) => {
     setUserId(event.target.value);
@@ -29,25 +39,26 @@ const App = () => {
   return (
     <div id="main">
       <label htmlFor="number">Enter an id for the user between 1 to 100</label>
-      <input
-        type="number"
-        value={userId}
-        onChange={onChangeHandler}
-        id="input"
-        min={1}
-        max={10}
-      />
+      <input type="number" value={userId} onChange={onChangeHandler} id="input" min={1} max={10} />
       <button id="btn" onClick={handleOnClick}>
         Get User
       </button>
 
       <div id="data">
-        <h1>Click on the button to get the user</h1>
-        <h4 id="id">{userData.id}</h4>
-        <h4 id="email">{userData.email}</h4>
-        <h4 id="name">{userData.name}</h4>
-        <h4 id="phone">{userData.phone}</h4>
-        <h4 id="website">{userData.website}</h4>
+        {isLoading === LoadingStatus.NOT_STARTED ? (
+          <h1>Click on the button to get the user</h1>
+        ) : isLoading === LoadingStatus.IN_PROGRESS ? (
+          <Loader />
+        ) : (
+          <>
+            <h1>Click on the button to get the user</h1>
+            <h4 id="id">{userData.id}</h4>
+            <h4 id="email">{userData.email}</h4>
+            <h4 id="name">{userData.name}</h4>
+            <h4 id="phone">{userData.phone}</h4>
+            <h4 id="website">{userData.website}</h4>
+          </>
+        )}
       </div>
     </div>
   );
